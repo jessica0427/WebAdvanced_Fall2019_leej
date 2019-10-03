c7749c118fbd92321641fd25f15695594a75c39a
jessica0427 <53446525+jessica0427@users.noreply.github.com>
2019-10-02 22:32:43 -0400

    JSON & Handlebars.js

--- AdvancedJavascript_web/assets/js/main.js
+++ AdvancedJavascript_web/assets/js/main.js
@@ -1,3 +1,26 @@
-;(function () {
-	
-	'use strict';
+window.addEventListener('DOMContentLoaded', function () {
+	loadData();
+
+});
+
+function loadData(){
+$.getJSON("../../data.json", function(data){
+
+		// console.log(data);
+
+		generateWebsites(data);
+
+});
+}
+
+function generateWebsites(){
+
+let source = $("#website-template").html();
+let template = Handlebars.compile(source);
+let result = template(data);
+let list = $(".website-list");
+
+list.append(result);
+
+
+}
